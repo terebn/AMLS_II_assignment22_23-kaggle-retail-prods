740457552ed8976a0b5d55aa86544c1cdda6963c
Teresa Bono <teresabono.t@gmail.com>
2023-03-27 23:03:25 +0100

    add bert to config and rename 'dataset's as 'dataset'

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config():
     # Project paths
     project_path = Path(__file__).parent
     out_path = project_path / 'out'
-    data_path = project_path / 'datasets'
+    data_path = project_path / 'dataset'
     model_path = project_path / 'models'
     hp_tuning_path = project_path / 'hp-tuning'
 
@@ -18,14 +18,19 @@ class Config():
 
     # CNN
 
-    # Tuning
+    # HP Tuning
     MAX_EPOCHS_HP = 50  # the maximum number of epochs to train one model. It is recommended to set this to a value slightly higher than the expected epochs to convergence for your largest Model FACTOR = 3  # the reduction factor for the number of epochs and number of models for each
     HYPERBAND_ITERATIONS = 1  # the number of times to iterate over the full Hyperband algorithm
     FACTOR = 3
     SUFFIX = '_augm'
-
     # Fitting
     EPOCHS = 100
+
+    # BERT
+
+    BERT_EPOCHS = 3
+    BERT_BATCH_SIZE = 4
+    BERT_SUFFIX = 'distilbert'
 
     @staticmethod
     def get_labels_map():
